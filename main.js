bec19166b6b050b5f47e3367d4023c842ea05ac2
Maelyn <120160610+maelynluraobs@users.noreply.github.com>
2023-01-10 17:46:27 +0800

    update @5;46pm

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
+/*heading */
+
+(function() {
+  const cursor = document.querySelector('.cursor');
+  
+  const innerCursor = document.querySelector('.inner-cursor');
+  
+  const followCursor = e => {
+    const { clientX: x, clientY: y} = e;
+    cursor.style.left = x + 'px';
+    cursor.style.top = y + 'px';
+    
+    innerCursor.style.left = x + 'px';
+    innerCursor.style.top = y + 'px';
+  }
+  
+  window.addEventListener('mousemove', followCursor);
+})();
 
 
-$(function(){
-  $("#nav-placeholder").load("nav.html");
-});
 /* CONTACT */
 document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.preventDefault();
